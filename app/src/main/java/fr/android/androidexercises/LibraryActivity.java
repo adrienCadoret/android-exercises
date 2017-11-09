947f4c8dd65554f67019b9ce55f2f8c0c413e989
unknown <adri.cadoret@gmail.com>
2017-11-09 10:32:10 +0100

    First exercise

--- app/src/main/java/fr/android/androidexercises/LibraryActivity.java
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.java
@@ -17,6 +17,7 @@ public class LibraryActivity extends AppCompatActivity {
 
         TextView messageTextView = (TextView) findViewById(R.id.messageTextView);
         // TODO call setText() on messageTextView
+        messageTextView.setText(R.string.home_string);
 
         setSupportActionBar(toolbar);
     }
