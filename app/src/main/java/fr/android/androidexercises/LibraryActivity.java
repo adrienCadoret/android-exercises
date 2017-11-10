41e3a328bf13ec52fa0fefc39dd90b38e1fea1f9
unknown <adri.cadoret@gmail.com>
2017-11-10 11:30:56 +0100

    OK

--- app/src/main/java/fr/android/androidexercises/LibraryActivity.java
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.java
@@ -1,5 +1,6 @@
 package fr.android.androidexercises;
 
+import android.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -10,11 +11,21 @@ public class LibraryActivity extends AppCompatActivity implements Step0Fragment.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_library);
         // TODO replace Step0Fragment in containerFrameLayout
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.containerFrameLayout, new Step0Fragment(), Step0Fragment.class.getSimpleName())
+                .commit();
     }
 
     @Override
-    public void onNext() {
+    public void onNext(String data) {
         // TODO replace Step1Fragment in containerFrameLayout
+        Step1Fragment fragment = new Step1Fragment();
+        fragment.setData(data);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.containerFrameLayout, fragment, Step1Fragment.class.getSimpleName())
+                .addToBackStack(Step1Fragment.class.getSimpleName())
+                .commit();
+
     }
 
 }
--- app/src/main/java/fr/android/androidexercises/Step1Fragment.java
+++ app/src/main/java/fr/android/androidexercises/Step1Fragment.java
@@ -1,9 +1,48 @@
 package fr.android.androidexercises;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class Step1Fragment extends Fragment {
 
     // TODO Override onCreateViewMethod
+    private TextView textView;
+    private String data;
 
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_step1, container, false);
+        textView = view.findViewById(R.id.textView);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        textView = view.findViewById(R.id.step1TextView);
+        textView.setText(data);
+    }
 }
