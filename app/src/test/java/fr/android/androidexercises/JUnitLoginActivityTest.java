5238642f7bd0bc2ac178e6076b0014a6b46fb04f
unknown <adri.cadoret@gmail.com>
2017-11-10 15:34:26 +0100

    OK

--- app/src/test/java/fr/android/androidexercises/JUnitLoginActivityTest.java
+++ app/src/test/java/fr/android/androidexercises/JUnitLoginActivityTest.java
@@ -1,5 +1,32 @@
 package fr.android.androidexercises;
 
+import org.assertj.android.api.Assertions;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+
+@RunWith(CustomRobolectricTestRunner.class)
 public class JUnitLoginActivityTest {
+
+    LoginActivity activity;
+
+    @Before
+    public void setUp() throws Exception{
+        activity = Robolectric.setupActivity(LoginActivity.class);
+    }
+
+    @Test
+    public void should_set__logged_state() throws Exception{
+        activity.logged();
+        Assertions.assertThat(activity.loginLayout).isGone();
+        Assertions.assertThat(activity.loggedText).isVisible();
+    }
+
+    @Test
+    public void should_set__unlogged_state() throws Exception{
+        activity.notLogged();
+        Assertions.assertThat(activity.loginLayout).isVisible();
+    }
 
 }
--- app/src/test/java/fr/android/androidexercises/LoginPresenterTest.java
+++ app/src/test/java/fr/android/androidexercises/LoginPresenterTest.java
@@ -1,5 +1,30 @@
 package fr.android.androidexercises;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
 public class LoginPresenterTest {
+
+    @Mock LoginActivity activity;
+    @InjectMocks LoginPresenter loginPresenter;
+
+    @Test
+    public void password_should_be_void() throws Exception{
+        loginPresenter.checkCredentials("password");
+        Mockito.verify(activity).logged();
+        Mockito.verify(activity).message(R.string.text_logged);
+    }
+
+    @Test
+    public void password_should_be_fail() throws Exception{
+        loginPresenter.checkCredentials("c");
+        Mockito.verify(activity).notLogged();
+        Mockito.verify(activity).message(R.string.notLogged);
+    }
 
 }
